0c22deb1e3d1a024b815200f69edc8751543dda5
RiccardoVoltolina <riky.volto@live.com>
2024-01-08 17:13:37 +0100

    put value in array

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
+// Prendo l'elemento nel mio html con id random_word e con innerHTML ci stampo un valore
 document.getElementById('random_world').innerHTML = 'ciao';
 
+// cerco nel mio browser un elemento salvato con il nome ‘random_word’. Se esiste, restituisce il suo valore come una stringa. Se non esiste, restituisce null. Con JSON.parse prendo una stringa in formato JSON e la converto in un oggetto JavaScript
 
+let random_word = JSON.parse(localStorage.getItem('random_word')) || [];
 
-// metto nel get l'url della chiamata
+// Metto nel get l'url della chiamata
 axios.get('https://flynn.boolean.careers/exercises/api/random/word')
   .then(function (response) {
-    // eseguo un consolle log per vedere la risposta
-    console.log(response);
+    // Eseguo un console log per vedere la risposta
+    console.log(response.data.response);
+    let single_word = response.data.response;
+    // inserisco la mia parola nell' array
+
+    random_word.push(single_word);
+
+    // salvo la stringa JSON nello storage locale del mio browser. I dati vengono salvati con la chiave 'random_word' con json.stringify converto l’array random_word in una stringa di testo in formato JSON.
+
+    localStorage.setItem('random_word', JSON.stringify(random_word));
+
+    console.log(random_word); 
   })
   .catch(function (error) {
-    // eseguo un consolle log per vedere gli errori
+    // Eseguo un console log per vedere gli errori
     console.log(error);
-  })
-  .finally(function () {
-    // always executed
   });
